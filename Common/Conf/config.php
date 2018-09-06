939f55bb25372509ec55aa44d89bde19d570469f
itxiaolong3 <1710600973@qq.com>
2018-09-06 23:06:20 +0800

    11
    111

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -16,8 +16,8 @@ return array(
     'DB_TYPE'            =>'mysql',         //数据库类型
     'DB_HOST'            =>'localhost',     //服务器地址
     'DB_NAME'            =>'douyin',        //数据库名
-    'DB_USER'            =>'root',          //用户名
-    'DB_PWD'             =>'123',           //密码
+    'DB_USER'            =>'douyin',          //用户名
+    'DB_PWD'             =>'134136110cjl',           //密码
     'DB_PORT'            =>'3306',          //端口
     'DB_PREFIX'          =>'dy_',           //数据表前缀
     'DB_PARAMS'          =>array(),         //数据库链接参数
